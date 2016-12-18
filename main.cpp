85ec3f65d8df69bd44672f55f619a8f509d9d557
elisakis <elisa.kis@yandex.ru>
2016-12-18 13:17:29 +0300

    Add files via upload
    9)	Создайте рекурсивную функцию, подсчитывающую сумму элементов массива по следующему алгоритму: массив делится пополам, подсчитываются и складываются суммы элементов в каждой половине. Сумма элементов в половине массива подсчитывается по тому же алгоритму, то есть снова путем деления пополам. Деления происходят, пока в получившихся кусках массива не окажется по одному элементу и вычисление суммы, соответственно, не станет тривиальным. 
    Input:
    7 - длина массива
    1 2 3 4 5 6 7 - элементы массива
    Output:
    28
    Комментарий: программа должна состоять из:
    - фун

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int strange_sum(int l, int arr[])
+{
+    int c = l/2;  //center
+
+    if (l == 0)
+        return 0;
+
+    if (l == 1)
+        return arr[0];
+    else
+    {
+        int* p1 = arr;  //pointer to 0 element
+        int* p2 = p1+c; //pointer to C element
+
+        int r1 = strange_sum(c, p1);
+        int r2 = strange_sum(l-c, p2);
+        return (r1 + r2);
+    }
+}
+
+
+int main()
+{
+    int l = 7;
+    int arr[l];
+
+    for (int i=0; i < l; i++)
+        cin >> arr[i];
+
+    cout << strange_sum(l, arr);
+}
